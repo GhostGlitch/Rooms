3d170670e60d532087bdbfe1c1b54f9fd73589fc
Ghost <Taylerobin@gmail.com>
2014-09-17 15:36:42 -0600

    Full Room Generation

--- src/rooms/controller/Rooms.java
+++ src/rooms/controller/Rooms.java
@@ -6,7 +6,7 @@ public class Rooms
 
 	public void RoomSetup()
 	{
-		while (RoomCount() < 10)
+		while (RoomCount() < 16)
 		{
 			for (int index = 0; index < 7; index++)
 			{
@@ -15,34 +15,60 @@ public class Rooms
 					Setup[index][index1] = false;
 				}
 			}
-			System.out.println();
 			Setup[3][3] = true;
-			while (!Setup[2][3] && !Setup[4][3] && !Setup[3][2] && !Setup[3][4])
-			{
-				Setup[2][3] = RanBool(.75);
-				Setup[4][3] = RanBool(.75);
-				Setup[3][2] = RanBool(.75);
-				Setup[3][4] = RanBool(.75);
-			}
-			Setup[2][2] = RanBool(getSurrounding(2, 2));
-			Setup[2][4] = RanBool(getSurrounding(2, 4));
-			Setup[4][2] = RanBool(getSurrounding(4, 2));
-			Setup[4][4] = RanBool(getSurrounding(4, 4));
-			Setup[3][1] = RanBool(getSurrounding(3, 1));
-			Setup[1][3] = RanBool(getSurrounding(1, 3));
+			Setup[2][3] = RanBool(.75);
+			Setup[4][3] = RanBool(.75);
+			Setup[3][2] = RanBool(.75);
+			Setup[3][4] = RanBool(.75);
 			Setup[5][3] = RanBool(getSurrounding(5, 3));
+			Setup[1][3] = RanBool(getSurrounding(1, 3));
+			Setup[6][3] = RanBool(getSurrounding(6, 3));
+			Setup[0][3] = RanBool(getSurrounding(0, 3));
 			Setup[3][5] = RanBool(getSurrounding(3, 5));
-			Setup[2][1] = RanBool(getSurrounding(2, 1));
-			Setup[1][2] = RanBool(getSurrounding(1, 2));
-			Setup[4][1] = RanBool(getSurrounding(4, 1));
-			Setup[1][4] = RanBool(getSurrounding(1, 4));
+			Setup[3][1] = RanBool(getSurrounding(3, 1));
+			Setup[3][6] = RanBool(getSurrounding(3, 6));
+			Setup[3][0] = RanBool(getSurrounding(3, 0));
+			Setup[4][2] = RanBool(getSurrounding(4, 2));
 			Setup[5][2] = RanBool(getSurrounding(5, 2));
-			Setup[2][5] = RanBool(getSurrounding(2, 5));
-			Setup[4][5] = RanBool(getSurrounding(4, 5));
+			Setup[6][2] = RanBool(getSurrounding(6, 2));
+			Setup[2][2] = RanBool(getSurrounding(2, 2));
+			Setup[1][2] = RanBool(getSurrounding(1, 2));
+			Setup[0][2] = RanBool(getSurrounding(0, 2));
+			Setup[4][4] = RanBool(getSurrounding(4, 4));
 			Setup[5][4] = RanBool(getSurrounding(5, 4));
+			Setup[6][4] = RanBool(getSurrounding(6, 4));
+			Setup[2][4] = RanBool(getSurrounding(2, 4));
+			Setup[1][4] = RanBool(getSurrounding(1, 4));
+			Setup[0][4] = RanBool(getSurrounding(0, 4));
+			Setup[2][5] = RanBool(getSurrounding(2, 5));
+			Setup[2][6] = RanBool(getSurrounding(2, 6));
+			Setup[2][1] = RanBool(getSurrounding(2, 1));
+			Setup[2][0] = RanBool(getSurrounding(2, 0));
+			Setup[4][5] = RanBool(getSurrounding(4, 5));
+			Setup[4][6] = RanBool(getSurrounding(4, 6));
+			Setup[4][1] = RanBool(getSurrounding(4, 1));
+			Setup[4][0] = RanBool(getSurrounding(4, 0));
+			Setup[1][1] = RanBool(getSurrounding(1, 1));
+			Setup[1][0] = RanBool(getSurrounding(1, 0));
+			Setup[0][1] = RanBool(getSurrounding(0, 1));
+			Setup[0][0] = RanBool(getSurrounding(0, 0));
+			Setup[1][5] = RanBool(getSurrounding(1, 5));
+			Setup[1][6] = RanBool(getSurrounding(1, 6));
+			Setup[0][5] = RanBool(getSurrounding(0, 5));
+			Setup[0][6] = RanBool(getSurrounding(0, 6));
+			Setup[5][1] = RanBool(getSurrounding(5, 1));
+			Setup[6][1] = RanBool(getSurrounding(6, 1));
+			Setup[5][0] = RanBool(getSurrounding(5, 0));
+			Setup[6][0] = RanBool(getSurrounding(6, 0));
+			Setup[5][5] = RanBool(getSurrounding(5, 5));
+			Setup[5][6] = RanBool(getSurrounding(5, 6));
+			Setup[6][5] = RanBool(getSurrounding(6, 5));
+			Setup[6][6] = RanBool(getSurrounding(6, 6));
 			System.out.println("          [" + RoomCount() + "]");
 			PrintSetup();
 		}
+		System.out.println("          [" + RoomCount() + "]");
+		PrintSetup();
 	}
 
 	private boolean RanBool(double Percent)
@@ -91,21 +117,33 @@ public class Rooms
 	private double getSurrounding(int index, int index1)
 	{
 		double roomCount = 0;
-		if (Setup[index + 1][index1])
+		if (index != 6)
 		{
-			roomCount = roomCount + .25;
+			if (Setup[index + 1][index1])
+			{
+				roomCount = roomCount + .25;
+			}
 		}
-		if (Setup[index - 1][index1])
+		if (index != 0)
 		{
-			roomCount = roomCount + .25;
+			if (Setup[index - 1][index1])
+			{
+				roomCount = roomCount + .25;
+			}
 		}
-		if (Setup[index][index1 + 1])
+		if (index1 != 6)
 		{
-			roomCount = roomCount + .25;
+			if (Setup[index][index1 + 1])
+			{
+				roomCount = roomCount + .25;
+			}
 		}
-		if (Setup[index][index1 - 1])
+		if (index1 != 0)
 		{
-			roomCount = roomCount + .25;
+			if (Setup[index][index1 - 1])
+			{
+				roomCount = roomCount + .25;
+			}
 		}
 		return roomCount;
 	}
