414dc925626652358ae3902f613475847af2bf92
Ghost <Taylerobin@gmail.com>
2014-09-19 10:19:08 -0600

    Cleaned up generation

--- src/rooms/controller/Rooms.java
+++ src/rooms/controller/Rooms.java
@@ -16,55 +16,46 @@ public class Rooms
 				}
 			}
 			Setup[3][3] = true;
-			Setup[2][3] = RanBool(getSurrounding(2, 3));
-			Setup[4][3] = RanBool(getSurrounding(4, 3));
-			Setup[3][2] = RanBool(getSurrounding(3, 2));
-			Setup[3][4] = RanBool(getSurrounding(3, 4));
-			Setup[5][3] = RanBool(getSurrounding(5, 3));
-			Setup[1][3] = RanBool(getSurrounding(1, 3));
-			Setup[6][3] = RanBool(getSurrounding(6, 3));
-			Setup[0][3] = RanBool(getSurrounding(0, 3));
-			Setup[3][5] = RanBool(getSurrounding(3, 5));
-			Setup[3][1] = RanBool(getSurrounding(3, 1));
-			Setup[3][6] = RanBool(getSurrounding(3, 6));
-			Setup[3][0] = RanBool(getSurrounding(3, 0));
-			Setup[4][2] = RanBool(getSurrounding(4, 2));
-			Setup[5][2] = RanBool(getSurrounding(5, 2));
-			Setup[6][2] = RanBool(getSurrounding(6, 2));
-			Setup[2][2] = RanBool(getSurrounding(2, 2));
-			Setup[1][2] = RanBool(getSurrounding(1, 2));
-			Setup[0][2] = RanBool(getSurrounding(0, 2));
-			Setup[4][4] = RanBool(getSurrounding(4, 4));
-			Setup[5][4] = RanBool(getSurrounding(5, 4));
-			Setup[6][4] = RanBool(getSurrounding(6, 4));
-			Setup[2][4] = RanBool(getSurrounding(2, 4));
-			Setup[1][4] = RanBool(getSurrounding(1, 4));
-			Setup[0][4] = RanBool(getSurrounding(0, 4));
-			Setup[2][5] = RanBool(getSurrounding(2, 5));
-			Setup[2][6] = RanBool(getSurrounding(2, 6));
-			Setup[2][1] = RanBool(getSurrounding(2, 1));
-			Setup[2][0] = RanBool(getSurrounding(2, 0));
-			Setup[4][5] = RanBool(getSurrounding(4, 5));
-			Setup[4][6] = RanBool(getSurrounding(4, 6));
-			Setup[4][1] = RanBool(getSurrounding(4, 1));
-			Setup[4][0] = RanBool(getSurrounding(4, 0));
-			Setup[1][1] = RanBool(getSurrounding(1, 1));
-			Setup[1][0] = RanBool(getSurrounding(1, 0));
-			Setup[0][1] = RanBool(getSurrounding(0, 1));
-			Setup[0][0] = RanBool(getSurrounding(0, 0));
-			Setup[1][5] = RanBool(getSurrounding(1, 5));
-			Setup[1][6] = RanBool(getSurrounding(1, 6));
-			Setup[0][5] = RanBool(getSurrounding(0, 5));
-			Setup[0][6] = RanBool(getSurrounding(0, 6));
-			Setup[5][1] = RanBool(getSurrounding(5, 1));
-			Setup[6][1] = RanBool(getSurrounding(6, 1));
-			Setup[5][0] = RanBool(getSurrounding(5, 0));
-			Setup[6][0] = RanBool(getSurrounding(6, 0));
-			Setup[5][5] = RanBool(getSurrounding(5, 5));
-			Setup[5][6] = RanBool(getSurrounding(5, 6));
-			Setup[6][5] = RanBool(getSurrounding(6, 5));
-			Setup[6][6] = RanBool(getSurrounding(6, 6));
-			// I know there's probably a much cleaner way of doing that.
+			for (int index = 3; index < 7; index++)
+			{
+				for (int index1 = 3; index1 < 7; index1++)
+				{
+					if (!(index == 3 && index1 == 3))
+					{
+						Setup[index][index1] = RanBool(getSurrounding(index, index1));
+					}
+				}
+			}
+			for (int index = 3; index >= 0; index--)
+			{
+				for (int index1 = 3; index1 >= 0; index1--)
+				{
+					if (!(index == 3 && index1 == 3))
+					{
+						Setup[index][index1] = RanBool(getSurrounding(index, index1));
+					}
+				}
+			}
+			for (int index = 2; index >= 0; index--)
+			{
+				for (int index1 = 4; index1 < 7; index1++)
+				{
+					if (!(index == 3 && index1 == 3))
+					{
+						Setup[index][index1] = RanBool(getSurrounding(index, index1));
+					}
+				}
+			}
+			for (int index = 4; index < 7; index++)
+			{
+				for (int index1 = 2; index1 >= 0; index1--)
+				{
+					if (!(index == 3 && index1 == 3))
+					{
+						Setup[index][index1] = RanBool(getSurrounding(index, index1));
+					}
+				}
+			}
 		}
 		System.out.println("          [" + RoomCount() + "]");
 		PrintSetup();
