9af5a6a8ce8c657a1c7db7efb0d45f982fc43305
GhostGlitch <taylerobin@gmail.com>
2014-09-26 09:55:59 -0600

    some work in Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,6 +1,19 @@
 package model;
 
+import java.util.Random;
+import model.Setup;
+
 public class Board
 {
-
+	public Board(Setup currentSetup)
+	{
+		
+	}
+	RanInt()
+	public int RanInt(int min, int max)
+	{
+		Random rand = new Random();
+		int ranNum = rand.nextInt((max - min) + 1) + min;
+		return ranNum;
+	}
 }
